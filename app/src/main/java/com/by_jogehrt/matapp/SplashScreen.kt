b20cb74d10d20a9d63ff73762f06a5ae1431db0c
BaudolinoII <espinodehorta99@gmail.com>
2023-10-20 20:53:59 -0600

    SplashScreen implementada con exito

--- app/src/main/java/com/by_jogehrt/matapp/SplashScreen.kt
+++ app/src/main/java/com/by_jogehrt/matapp/SplashScreen.kt
@@ -1,11 +1,19 @@
 package com.by_jogehrt.matapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.concurrent.thread
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        thread{
+            Thread.sleep(3000)
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
     }
 }
